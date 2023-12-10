b556888bfa1d28aaa09ad56cccc2a565266cab74
orlando.hernandez <orlando.hernandez@sisamex.com.mx>
2023-12-09 20:48:29 -0600

    Creo indice de censo

--- models/censo_m.go
+++ models/censo_m.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 // Censo definicion de la tabla
 type Censo struct {
 	gorm.Model
-	CasinoID          uint   `json:"casino_id"`
+	CasinoID          uint   `json:"casino_id" gorm:"index:idx_censo_casino,unique"`
 	Casino            Casino `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	SistemaPrincipal  string `json:"sistema_principal" gorm:"type:character varying(250);"`
 	SistemaSecunadrio string `json:"sistema_Secundario" gorm:"type:character varying(250);"`
